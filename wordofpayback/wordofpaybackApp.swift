e438ed4dda011a0d821c602b0e562836bc195abd
TsimurAsayonak <tsimur.asayonak@nordsec.com>
2023-12-18 19:33:14 +0100

    app init

---
+++ wordofpayback/wordofpaybackApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordofpaybackApp.swift
+//  wordofpayback
+//
+//  Created by Tsimur Asayonak on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct wordofpaybackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
