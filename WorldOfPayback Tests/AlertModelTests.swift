f3d1fa1f18e3f567b7fcdb17a4a16f8ab5108e73
TsimurAsayonak <tsimur.asayonak@nordsec.com>
2023-12-26 09:53:34 +0100

    added test module

---
+++ WorldOfPayback Tests/AlertModelTests.swift
@@ -0,0 +1,13 @@
+//
+//  AlertModelTests.swift
+//  WorldOfPayback Tests
+//
+//  Created by Tsimur Asayonak on 12/26/23.
+//
+
+import XCTest
+
+final class AlertModelTests: XCTestCase {
+    func testExample() throws {
+    }
+}
