f4e1afb0d2f57e6da5a74318d3e6411e13c95196
TsimurAsayonak <tsimur.asayonak@nordsec.com>
2023-12-26 13:02:54 +0100

    added TransactionValueTests

--- WorldOfPayback Tests/Models/Transaction/TransactionValueTests.swift
+++ WorldOfPayback Tests/Models/Transaction/TransactionValueTests.swift
@@ -5,31 +5,40 @@
 //  Created by Tsimur Asayonak on 12/26/23.
 //
 
+@testable import WorldOfPayback_Dev
 import XCTest
 
 final class TransactionValueTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testAllValues() {
+        let sut = TransactionValue.sut
+        
+        XCTAssertEqual(sut.amount, 1)
+        XCTAssertEqual(sut.currency, .PBP)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testFormattedValue() {
+        let sut = TransactionValue.sut
+        
+        XCTAssertEqual(sut.formattedValue, "1 PBP")
     }
+}
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+extension TransactionValue {
+    static var sut: Self {
+        .init(amount: 1, currency: .PBP)
     }
+}
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+struct TransactionValue: Codable, Equatable, Hashable {
+    var amount: Int?
+    var currency: Currency?
+    
+    var formattedValue: String {
+        "\(amount ?? 0) \(currency?.rawValue ?? "")"
     }
-
+    
+    enum CodingKeys: String, CodingKey {
+        case amount
+        case currency
+    }
 }
