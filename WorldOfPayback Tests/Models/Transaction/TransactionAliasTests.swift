c01c0c6ef3b67e011f27ecb230b910b5a4c3616d
TsimurAsayonak <tsimur.asayonak@nordsec.com>
2023-12-26 13:12:48 +0100

    TransactionAliasTests

--- WorldOfPayback Tests/Models/Transaction/TransactionAliasTests.swift
+++ WorldOfPayback Tests/Models/Transaction/TransactionAliasTests.swift
@@ -5,31 +5,13 @@
 //  Created by Tsimur Asayonak on 12/26/23.
 //
 
+@testable import WorldOfPayback_Dev
 import XCTest
 
 final class TransactionAliasTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testAllValues() {
+        let sut = TransactionAlias(reference: "reference")
+        
+        XCTAssertEqual(sut.reference, "reference")
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
