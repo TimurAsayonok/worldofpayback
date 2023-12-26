d57641d3fa097d58aef2ea3e5881b3e89b9e0348
TsimurAsayonak <tsimur.asayonak@nordsec.com>
2023-12-26 13:07:28 +0100

    TransactionDetailsTests

--- WorldOfPayback Tests/Models/Transaction/TransactionDetailsTests.swift
+++ WorldOfPayback Tests/Models/Transaction/TransactionDetailsTests.swift
@@ -5,31 +5,32 @@
 //  Created by Tsimur Asayonak on 12/26/23.
 //
 
+@testable import WorldOfPayback_Dev
 import XCTest
 
 final class TransactionDetailsTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testAllValues() {
+        let testDate = Date()
+        let sut = TransactionDetail.sut(date: testDate)
+        
+        XCTAssertEqual(sut.description, "description")
+        XCTAssertEqual(sut.bookingDate, testDate)
+        XCTAssertEqual(sut.value, .sut)
     }
+}
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+extension TransactionDetail {
+    static func sut(date: Date) -> Self {
+        .init(
+            description: "description",
+            bookingDate: date,
+            value: .sut
+        )
     }
+}
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+struct TransactionDetail: Codable, Equatable, Hashable {
+    var description: String?
+    var bookingDate: Date?
+    var value: TransactionValue?
 }
