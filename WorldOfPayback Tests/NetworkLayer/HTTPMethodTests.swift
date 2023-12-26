4cb89ec5990a17ea72dbc5fdf1e3fbd5edbe0149
TsimurAsayonak <tsimur.asayonak@nordsec.com>
2023-12-26 13:51:11 +0100

    HTTPMethodTests

--- WorldOfPayback Tests/NetworkLayer/HTTPMethodTests.swift
+++ WorldOfPayback Tests/NetworkLayer/HTTPMethodTests.swift
@@ -5,31 +5,15 @@
 //  Created by Tsimur Asayonak on 12/26/23.
 //
 
+@testable import WorldOfPayback_Dev
 import XCTest
 
 final class HTTPMethodTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testAllCase() {
+        let sut: [HTTPMethod] = [.get, .post, .put, .delete]
+        
+        sut.forEach {
+            XCTAssertEqual($0, HTTPMethod(rawValue: $0.rawValue))
         }
     }
-
 }
