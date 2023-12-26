1e7cd5903a08d750ca650b3083055e85c540cb72
TsimurAsayonak <tsimur.asayonak@nordsec.com>
2023-12-26 10:13:47 +0100

    Error response tests

---
+++ WorldOfPayback Tests/Models/Common/ErrorResponseTests.swift
@@ -0,0 +1,24 @@
+//
+//  ErrorResponseTests.swift
+//  WorldOfPayback Tests
+//
+//  Created by Tsimur Asayonak on 12/26/23.
+//
+
+@testable import WorldOfPayback_Dev
+import XCTest
+
+final class ErrorResponseTests: XCTestCase {
+    func testMustInitWithAllValues() {
+        let sut = ErrorResponse.mock
+        XCTAssertNotNil(sut)
+        XCTAssertNotNil(sut.message)
+    }
+
+}
+
+extension ErrorResponse {
+    static var mock: Self {
+        .init(message: "error")
+    }
+}
